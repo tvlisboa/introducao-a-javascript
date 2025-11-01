a8930de46a77a18161a93a05e36238b67ec26d9c
tvlisboa <tvlisboa@hotmail.com>
2025-11-01 18:07:58 -0300

    introducao a while e do_while

---
+++ aula13/ambiente.js
@@ -0,0 +1,27 @@
+console.log("teste1")
+
+var cont = 0;
+
+/**
+ * Estrutura de repeticao 
+ * com teste logico no 
+ * inicio
+ * teste - executa - looping
+ */
+
+while (cont < 10) {
+    console.log("teste2")
+    cont++
+}
+
+/**
+ * Estrutura de repeticao 
+ * com teste logico no 
+ * final
+ * executa - testa - looping
+ */
+
+do {
+    cont++
+    console.log("teste3")
+} while (cont < 10)
