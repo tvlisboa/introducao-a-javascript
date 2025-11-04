363387c1a4fcd0f2cf5b3f381ec6152d7bac0f98
tvlisboa <tvlisboa@hotmail.com>
2025-11-03 22:03:16 -0300

    introducao a loopings

---
+++ aula14-looping/ambiente.js
@@ -0,0 +1,15 @@
+var cont = 0;
+while (cont < 10) {
+    console.log(cont)
+    cont++
+}
+
+var cont1 = 0;
+do {
+    console.log(cont1)
+    cont1++
+} while (cont1 <= 10)
+
+for (cont2 = 0; cont2 <= 5; cont2++) {
+    console.log(cont2)
+}
