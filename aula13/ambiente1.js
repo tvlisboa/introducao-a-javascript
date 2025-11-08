1b7066948b30c5b0e019d72e818f3557644de4eb
tvlisboa <tvlisboa@hotmail.com>
2025-11-08 15:27:43 -0300

    exercicios com for e looping

---
+++ aula13/ambiente1.js
@@ -0,0 +1,26 @@
+
+var cont = 5;
+
+/**
+ * Teste logico no inicio
+ */
+
+while (cont < 10) {
+    console.log(`Passo ${cont} `)
+    cont++
+}
+
+/**
+ * Teste logico no final
+ */
+
+do {
+    cont++
+    console.log(" ")
+    console.log(`Passo ${cont}`)
+} while (cont < 12)
+
+for (let i = 0; i < 10; i++) {
+    console.log(`Passo ${cont}`)
+
+}
---
+++ aula13/ambiente_for.js
@@ -0,0 +1,8 @@
+for (var cont = 1; cont < 10; cont++) {
+    console.log(`Valores ${cont}`)
+    if (cont % 2 == 0) {
+        console.log(`Numero par ${cont}`)
+    } else {
+        console.log(`Numero Impar ${cont}`)
+    }
+}
