32956ee726893d733f72917b0123670dca8a297b
tvlisboa <tvlisboa@hotmail.com>
2025-11-14 22:09:15 -0300

    exercicios com javascript

---
+++ aula13/ambienteteste.js
@@ -0,0 +1,19 @@
+/**console.log("Testando loops")
+console.log("Testando loops")
+console.log("Testando loops")
+console.log("Testando loops")
+console.log("Testando loops")
+**/
+
+var cont = 1;
+
+while (cont <= 10) {
+    console.log("Teste de loops - while")
+    cont++;
+}
+
+do {
+    console.log("Testando loops com - do_while")
+    cont++
+} while (cont <= 100)
+
