40c93e2a76e7820b7a51f98994099dea614f71aa
tvlisboa <tvlisboa@hotmail.com>
2025-11-24 21:55:11 -0300

    novos exemplos com while e do_while

---
+++ aula13/while_exemplo.js
@@ -0,0 +1,26 @@
+console.log("Correr maratona")
+
+
+var correr1 = 0;
+var correr2 = 0;
+
+/**
+ * Estrutura de repeticao
+ * com teste logico no 
+ * inicio
+ */
+
+while (correr1 < 42) {
+    console.log("Correndo uma maratona")
+    correr1++
+}
+
+console.log("Voce chegou ao fim")
+
+
+do {
+    console.log("Correndo uma meia maratona")
+    correr2++
+} while (correr2 < 21);
+
+console.log("saida")
